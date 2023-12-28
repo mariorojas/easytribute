223df078cacbcb323e121e797e11d4ae9de0a90f
Mario Rojas <mrojas.leyva@gmail.com>
2023-12-27 23:46:13 -0600

    disable temporary email login

--- magic_links/forms.py
+++ magic_links/forms.py
@@ -1,5 +1,18 @@
 from django import forms
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+
+from . import blacklist
 
 
 class EmailLoginForm(forms.Form):
     email = forms.EmailField()
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not settings.TEMPORARY_EMAIL_LOGIN:
+            domain = email.split('@')[1]
+            if domain in blacklist.DOMAINS:
+                raise ValidationError(_('Temporary emails are not allowed'))
+        return email
