234ba4f54d86397d6f5446c27bd6a1a3f09d9178
Mario Rojas <mrojas.leyva@gmail.com>
2024-04-03 23:08:44 -0600

    increase user created tributes to 10

--- tributes/views.py
+++ tributes/views.py
@@ -127,7 +127,7 @@ class TributeHomeView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        latest = Tribute.objects.filter(active=True).order_by('-created_at')[:5]
+        latest = Tribute.objects.filter(active=True).order_by('-created_at')[:10]
         context.update({'latest': latest})
         return context
 
